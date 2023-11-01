4d9f9298c1099176f70f737cccfc6c73e36d5a98
Fedi Beltaief <fediblt3@gmail.com>
2023-11-01 21:47:23 +0100

    Part3

--- app/src/main/java/com/example/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.droidcafe;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -33,8 +36,18 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
 
+        setSupportActionBar(binding.toolbar);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayMap();
+            }
+        });
 
     }
     public void displayToast(String message) {
@@ -48,22 +61,29 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_order) {
+            displayToast(getString(R.string.action_order_message));
             return true;
+        } else if (item.getItemId() == R.id.action_status) {
+            displayToast(getString(R.string.action_status_message));
+            return true;
+        } else if (item.getItemId() == R.id.action_favorites) {
+            displayToast(getString(R.string.action_favorites_message));
+            return true;
+        } else if (item.getItemId() == R.id.action_contact) {
+            displayToast(getString(R.string.action_contact_message));
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
+
 
     public void showDonutOrder(View view) {
         showFoodOrder(getString(R.string.donut_order_message));
@@ -76,5 +96,14 @@ public class MainActivity extends AppCompatActivity {
         showFoodOrder(getString(R.string.froyo_order_message));
     }
 
+    public void displayMap() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        String data = getString(R.string.google_mtv);
+        intent.setData(Uri.parse(data));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
 }
--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -2,6 +2,8 @@ package com.example.droidcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -41,4 +43,5 @@ public class OrderActivity extends AppCompatActivity {
             Log.d(TAG_ACTIVITY, getString(R.string.nothing_clicked));
         }
     }
+
 }
